5782dc14e6184c26efe5eea932a46fbd4d91f9c0
Lilly Hackworth <lilly@hackworth.org>
2019-05-17 12:51:57 -0500

    Basic darksky call

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import DarkSkyApi from 'dark-sky-api'
 import { theme } from '@instructure/canvas-theme'
 import { Text, Heading } from '@instructure/ui-elements'
 import { Button } from '@instructure/ui-buttons'
@@ -33,8 +34,15 @@ class App extends React.Component {
   }
 
   async componentDidMount () {
+    DarkSkyApi.apiKey = '063be74a6b9691f10b7c4e43f2f642af';
+    const position = {
+      latitude: 41.78468745,
+      longitude: -87.60074932651055
+    };
+    DarkSkyApi.loadCurrent(position)
+      .then(result => console.log(result));
 
-    
+
   }
 
   render () {
@@ -44,7 +52,7 @@ class App extends React.Component {
       <div>
         <Heading>Hello!</Heading>
         {!details && <div>
-          <Button 
+          <Button
             size="large"
             onClick={this.goForward}
           >Tap to learn more</Button>
